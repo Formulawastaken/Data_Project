2ff24f0aa59b6662ae5d4d02a0da4fea33d2ae13
Kemuel05 <kemuel.cubero@upr.edu>
2025-04-16 00:06:26 -0400

    getters, setters & constructor for vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+import java.time.*;
+public class Vehicle {
+    private int id;
+    private String type;
+    private String description;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String place;
+    //constructor
+    public Vehicle(String type, String description, String startTime, String endTime, String place){
+        Random random = new Random();
+        this.id = random.nextInt(1000); //setting a random integer between 0-1000 for unique id
+        this.type=type;
+        this.description=description;
+        String[] startTimeSplit=startTime.split(":"); //splits the start time into hour and minutes
+        String[] endTimeSplit=endTime.split(":"); //splits the end time into hour and minutes
+        this.startTime = LocalTime.of(Integer.parseInt(startTimeSplit[0]), Integer.parseInt(startTimeSplit[1]));  
+        this.endTime = LocalTime.of(Integer.parseInt(endTimeSplit[0]), Integer.parseInt(endTimeSplit[1]));
+        this.place=place;
+    }
+    //setters 
+    public void setDescription(String description){
+        this.description=description;
+    }
+    public void setType(String type){
+        this.type=type;
+    }
+    public void setStartTime(String startTime){
+        String[] startTimeSplit=startTime.split(":");
+        this.startTime = LocalTime.of(Integer.parseInt(startTimeSplit[0]), Integer.parseInt(startTimeSplit[1]));
+    }
+    public void setEndTime(String endTime){
+        String[] endTimeSplit=endTime.split(":");
+        this.endTime = LocalTime.of(Integer.parseInt(endTimeSplit[0]), Integer.parseInt(endTimeSplit[1]));
+    }
+    public void setPlace(String place){
+        this.place=place;
+    }
+    
+    //getters
+    public String getDescription(){
+        return this.description;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public LocalTime getStartTime(){
+        return this.startTime;
+    }
+    public LocalTime getEndTime(){
+        return this.endTime;
+    }
+    public String getPlace(){
+        return this.place;
+    }
+
+    //methods
+    
+}
