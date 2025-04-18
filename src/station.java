3769eaf43bd94b69de96c2b23c2b0a8331e38ed2
sebastianramirez5 <sebastian.ramirez5@upr.edu>
2025-04-18 15:33:12 -0400

    added vehicle cmds to station class

--- src/station.java
+++ src/station.java
@@ -1,11 +1,13 @@
 import java.time.*;
 import java.util.*;
+//should everything be static? i think only the user list should be static bc everything else is proprietary to each station no? 
 public class station {
     public static Set<User> users = new HashSet<>(); //current registered users
     public static Set<Vehicle> availableVehicles = new HashSet<>(); //available vehicles for clients
     public static Map<User,Vehicle> reservations = new HashMap<>(); //pairs clients with the veichle they reserved 
     public static Queue<User> waitingList = new LinkedList<>();//waiting queue for clients when no vehicles are available
 
+    //user cmds
     public static void addUser(String name,String email,String type,String numEst,String numTel){
         User newUser = new User(name, email, type, numEst, numTel);
         users.add(newUser);
@@ -61,6 +63,61 @@ public class station {
         scanner.close();
     }
 
+    //vehicle cmds
+    public static void addVehicle(String type, String description, String startTime, String endTime, String place ){
+        availableVehicles.add(new Vehicle(type,description,startTime,endTime, place));
+    }
+    public static void removeVehicle(int id){
+        if(!availableVehicles.isEmpty()){
+            Vehicle vehicle=null;
+            for(Vehicle v:availableVehicles){
+                if(v.getId()==id){
+                    vehicle=v;
+                    break;
+                } 
+            }
+            availableVehicles.remove(vehicle);
+        }
+        else{
+            System.out.println("There are currently no users to modify, please add one first.");
+        }
+
+    }
+    public static void modifyVehicle(int id, String command){
+        if(!availableVehicles.isEmpty()){
+            Vehicle vehicle=null;
+            Scanner scanner=new Scanner(System.in);
+            for(Vehicle v:availableVehicles){
+                if(v.getId()==id){
+                    vehicle=v;
+                    break;
+                }
+            }
+            switch(command){
+
+                case "Type":
+                System.out.println("Input new type for the vehicle");
+                vehicle.setType(scanner.nextLine());
+                scanner.close();
+
+                case "Description":
+
+                System.out.println("Input new description for the vehicle");
+                vehicle.setDescription(scanner.nextLine());
+                scanner.close();
+
+                case "Place":
+
+                System.out.println("Input new place for the vehicle");
+                vehicle.setDescription(scanner.nextLine());
+                scanner.close();
+                //TODO start and end time modifiers, should owner be able to be modified?
+            }
+        }
+        else{
+            System.out.println("There are currently no vehicles to modify, please add one first.");
+        }
+    }
 
 
 
